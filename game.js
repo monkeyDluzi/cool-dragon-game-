d52e5ad8064c11b51ae28e4be996506d08c1f1ee
monkeyDluzi <Luziandre588@gmail.com>
2026-06-24 20:01:26 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,26 @@
+const player = document.getElementById("player");
 
+let x = 100;
+let y = 300;
+
+document.addEventListener("keydown", (event) => {
+
+    if (event.key === "ArrowRight") {
+        x += 20;
+    }
+
+    if (event.key === "ArrowLeft") {
+        x -= 20;
+    }
+
+    if (event.key === "ArrowUp") {
+        y -= 20;
+    }
+
+    if (event.key === "ArrowDown") {
+        y += 20;
+    }
+
+    player.style.left = x + "px";
+    player.style.top = y + "px";
+});
